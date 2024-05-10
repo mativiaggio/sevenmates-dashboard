cb20949abe405b30aadabcb64fe898ec5d61c9b7
Matias Viaggio <matiasviaggio59@gmail.com>
2024-05-10 14:54:27 -0300

    Admins - v1.0.3

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/lib/db";
+import { mongooseConnect } from "@/lib/mongoose";
 
 import { Admin } from "@/models/Admin";
 
@@ -17,6 +18,7 @@ const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     session: async ({ session, token, user }) => {
+      await mongooseConnect();
       const admins = await Admin.find({}, "email");
       const adminEmails2 = admins.map((admin) => admin.email);
       console.log("Estos son los emails: ", adminEmails2);
@@ -35,6 +37,7 @@ export default NextAuth(authOptions);
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
 
+  await mongooseConnect();
   const admins = await Admin.find({}, "email");
   const adminEmails2 = admins.map((admin) => admin.email);
   console.log("Estos son los emails: ", adminEmails2);
